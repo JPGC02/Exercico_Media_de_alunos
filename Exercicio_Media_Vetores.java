7aef9c6b043759fad76c6126060c1c97f6ccb6c6
JPGC02 <103973489+JPGC02@users.noreply.github.com>
2022-06-13 22:51:43 -0300

    Update Exercicio_Media_Vetores.java

--- Exercicio_Media_Vetores.java
+++ Exercicio_Media_Vetores.java
@@ -1,57 +1,47 @@
 import java.util.Scanner;
 
- 
+
 import java.util.Arrays;
 
-public class Exercicio_Media_Vetores
-{
-  
- 
-public static void main (String[]args)
-  {
-    
-Scanner Alunos = new Scanner (System.in);
-    
- 
-      // Declaracao das variaveis e de vetor com valor definido para 6 modulos 
-    int soma = 0, q[] = new int[6];
-    
-float media = 0;
-    
- 
-      // Estrutura de repeticao numero do valor definido do vetor
-      for (int i = 0; i < q.length; i++)
-      {
-	
-	  // Pedir ao usuario um valor (dados) e somar 1 na contagem
-	  System.out.printf ("Digite a quantidade de alunos da Sala %d\n",
-			     (i + 1));
-	
-	  // Guardar o valor recebido no vetor de acordo com o valor somado da contagem (i)
-	  q[i] = Alunos.nextInt ();
-	
-	  // Receber na variavel um valor somado do vetor
-	  soma = soma + q[i];
-	
-	  // Receber na variavel um valor dividido para tirar a media
-	  media = soma / q.length;
-      
-} 
-      // Ordenar o valor maior e o menor que estao dentro do vetor
-      Arrays.sort (q);
-    
-      // Declaracao de variaveis do vetor, maior valor e menor valor
-    int maior = q[5];
-    
-int menor = q[0];
-    
- 
-      // Exibir ao usuario os dados de media das salas, a sala com maior e menor numero de alunos
-      System.out.printf ("A media de alunos das salas e: %f\n", media);
-    
-System.out.printf ("A sala com mais alunos tem %d alunos\n", maior);
-    
-System.out.printf ("A sala com menos alunos tem %d alunos\n", menor);
+public class Exercicio_Media_Vetores {
 
-} 
+
+    public static void main(String[] args) {
+
+        Scanner estudantes = new Scanner(System.in);
+
+
+        // Declaracao das variaveis e vetor
+        int total = 0, g[] = new int[6];
+        float mediania = 0;
+
+
+        // Estrutura de repeticao numero do valor definido do vetor
+        for (int i = 0; i < g.length; i++) {
+
+            // dados do usuario
+            System.out.printf("Digite a quantidade de alunos da Sala %d\n",
+                (i + 1));
+
+            // armazenar o valor recebido no vetor
+            g[i] = estudantes.nextInt();
+            total = total + g[i];
+            mediania = total / g.length;
+        }
+        // determinar o valor maior e o menor
+        Arrays.sort(g);
+
+        // Declaracao de variaveis maior e menor
+        int maior = g[5];
+        int menor = g[0];
+
+
+        // Exibir os dados da media das salas, a sala com maior e menor de alunos
+        System.out.printf("A media de alunos das salas e: %f\n", mediania);
+
+        System.out.printf("A sala com a maior quantidade de alunos tem %d alunos\n", maior);
+
+        System.out.printf("A sala com a menor quantidade de alunos tem %d alunos\n", menor);
+
+    }
 }
